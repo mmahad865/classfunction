7cbbccf80044f9dc2a9b48c0d921d542d7678c36
mahad Ali <mmahad865@gmail.com>
2022-08-21 12:11:40 -0700

    saved 4th

--- js/app.js
+++ js/app.js
@@ -132,7 +132,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+let 
 }
 
 
